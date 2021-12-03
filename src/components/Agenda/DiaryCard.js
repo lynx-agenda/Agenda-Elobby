231a5336710f5ae27279030c4cb46fb0656160b4
Marcos <marcoslorpen19@gmail.com>
2021-12-03 16:33:51 +0000

    navegacion a elemento desde agenda

--- src/components/Agenda/DiaryCard.js
+++ src/components/Agenda/DiaryCard.js
@@ -1,6 +1,6 @@
 import './Agenda.css'
 import { Image } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function ImageByMovieDB(posterPath) {
   return `https://image.tmdb.org/t/p/original/${posterPath}`;
@@ -8,12 +8,29 @@ function ImageByMovieDB(posterPath) {
 
 export default function DiaryCard(props) {
     let elemento = props.elemento;
-  return (
-    <Link to='#' className="list-item">
+    let navigate = useNavigate();
+    let viewURL = '';
+    if(elemento.type == 'game'){
+      viewURL = `/Games/View/${elemento.id}`
+    }
+    if(elemento.type == 'tv'){
+      viewURL = `/TV/View/${elemento.id}`
+    }
+    if(elemento.type == 'movie'){
+      viewURL = `/Movies/View/${elemento.id}`
+    }
+
+    const handleClick = () => {
+      navigate(viewURL)
+      console.log(viewURL)
+    }
+
+    return (
+    <div onClick={handleClick} className="list-item">
       {elemento.poster_path!==undefined ? <Image src={ImageByMovieDB(elemento.poster_path)} fluid/> : <Image src={elemento.background_image} fluid/>}
       <div className="overlay">
         {elemento.name!==undefined ? <h1>{elemento.name}</h1> : <h1>{elemento.original_title}</h1>}
       </div>
-    </Link>
+    </div>
   );
 }
