6721afceb45f34b5258b94814a18c0ddbe97171e
Alejandro Rodriguez <kroalca@gmail.com>
2021-12-03 17:44:30 +0000

    Solución de errores

--- src/services/getFromThirdApis.js
+++ src/services/getFromThirdApis.js
@@ -14,6 +14,7 @@ function getBooksFromThird({
   langRestrict = "es", // Restricts the volumes returned to those that are tagged with the specified language
   printType = "all", // Does not restrict by print type
   startIndex = 0, // The position in the collection at which to start. The index of the first item is 0.
+  typeElobby= "",
   fields = "totalItems,items(id,volumeInfo(title,authors,publisher,publishedDate,description,industryIdentifiers,pageCount,categories,imageLinks,language))", // Filter search results by volume type and availability.
 } = {}) {
 
@@ -34,10 +35,13 @@ function getBooksFromThird({
       return res.json();
     })
     .then((res) => {
+      res.typeElobby = typeElobby;
       return res;
     })
     .catch((error) => console.log(error));
 }
+
+
 
 function getFromTheMovieDB({
   query = "",
